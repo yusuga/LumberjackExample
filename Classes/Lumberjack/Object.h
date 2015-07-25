6ae24ff957f7258b97a835b05620eb96a3bd3147
Yu Sugawara <dev@yusuga.com>
2015-07-25 18:26:09 +0900

    Initial commit

---
+++ Classes/Lumberjack/Object.h
@@ -0,0 +1,13 @@
+//
+//  Object.h
+//  LumberjackExample
+//
+//  Created by Yu Sugawara on 7/25/15.
+//  Copyright (c) 2015 Yu Sugawara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Object : NSObject
+
+@end
---
+++ Classes/Lumberjack/Object.m
@@ -0,0 +1,23 @@
+//
+//  Object.m
+//  LumberjackExample
+//
+//  Created by Yu Sugawara on 7/25/15.
+//  Copyright (c) 2015 Yu Sugawara. All rights reserved.
+//
+
+#import "Object.h"
+#import <LumberjackLauncher/LumberjackLauncher.h>
+
+@implementation Object
+
+- (instancetype)init
+{
+    DDLogDebug(@"DDLogDebug");
+    if (self = [super init]) {
+        
+    }
+    return self;
+}
+
+@end
